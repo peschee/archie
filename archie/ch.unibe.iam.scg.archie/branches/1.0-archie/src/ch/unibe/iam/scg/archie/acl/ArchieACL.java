5f1fb19ad8494893985b16706546d1009fe2a63f
hephster <hephster@a31abdba-c511-11dd-9d8b-4f985c3f9f46>
2009-01-21 16:14:59 +0000

    This branch will be released with elexis 1.4 and is called archie 1.0. The trunk version will be released with the next elexis version, probably 2.0.

---
+++ archie/ch.unibe.iam.scg.archie/branches/1.0-archie/src/ch/unibe/iam/scg/archie/acl/ArchieACL.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Dennis Schenk, Peter Siska.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dennis Schenk - initial implementation
+ *     Peter Siska	 - initial implementation
+ *******************************************************************************/
+package ch.unibe.iam.scg.archie.acl;
+
+import ch.elexis.Hub;
+import ch.elexis.admin.ACE;
+import ch.elexis.admin.IACLContributor;
+import ch.unibe.iam.scg.archie.ArchieActivator;
+import ch.unibe.iam.scg.archie.i18n.Messages;
+
+/**
+ * <p>Handles the access to Archie based on access control lists defined in Elexis
+ * properties.<br/>
+ * TODO: revert ACE changes, they are only implemented in elexis 2.0. Also implement right localization</p>
+ * 
+ * $Id$
+ * 
+ * @author Peter Siska
+ * @author Dennis Schenk
+ * @version $Rev$
+ */
+public class ArchieACL implements IACLContributor {
+
+	/**
+	 * Access control string that will be displayed in the ACL.
+	 */
+	public static final ACE USE_ARCHIE = new ACE(ACE.ACE_ROOT, ArchieActivator.PLUGIN_NAME + " " + Messages.ACL_ACCESS);
+
+	/**
+	 * Returns the ACL for this plugin.
+	 * 
+	 * @return String[]
+	 */
+	public ACE[] getACL() {
+		// TODO: localization?
+		return new ACE[] { ArchieACL.USE_ARCHIE };
+	}
+
+	/** {@inheritDoc} */
+	public ACE[] reject(final ACE[] acl) {
+		return null;
+	}
+
+	/**
+	 * Static function to check whether the currently active user has access to
+	 * archie or not.
+	 * 
+	 * @return boolean True if the current user can access archie, false else.
+	 */
+	public static boolean userHasAccess() {
+		return Hub.actUser != null && Hub.actMandant != null && Hub.acl.request(ArchieACL.USE_ARCHIE);
+	}
+
+}
